455a023d69a6df270b24bb862d95f348e5f32c66
Kushagra Mehrotra <68564934+kushaagr@users.noreply.github.com>
2023-10-03 17:48:55 +0530

    Update file

--- mdyaml-parser.py
+++ mdyaml-parser.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from lark import Lark, Transformer, v_args
 
 from pprint import pprint
+
 os.chdir(Path(__file__).parent)
 if 'dev' in os.environ.keys():
     sys.path.append(os.environ['dev'] + '\\Python\\')
@@ -17,8 +18,18 @@ else:
 
 
 class FroMaAstTransformer(Transformer):
-    TEXT_LINE = BLANK_LINE = str
-    PROPERTY = KEY_NAME = STRING = lambda _, x: x.strip()
+    TEXT_LINE = BLANK_LINE = EMPTY_LINE = lambda _, l: "".join(l)
+    text_line = blank_line = empty_line = lambda _, l: "".join(l)
+    LINE_OF_TEXT = ENDL = lambda _, l: "".join(l)
+    # PROPERTY = CNAME = STRING = lambda _, x: str(x).strip()
+    STRING = lambda _, x: str(x).strip()
+    # key_name = lambda _, x: str(x).strip()
+    # KEY_NAME = key_name = lambda _, x: print(f"KEY_NAME details: {x} {type(x)} str(x): {str(x)}") or str(x).strip()
+    KEY_NAME = key_name = lambda _, x: str(x)
+    # property = lambda _, x: str(x)
+    DETECT_NO_FRONTMATTER = str
+    first_line = lambda self, lines: { "header" : "".join(lines) }
+
     NUMBER = float
     boolean = bool
     true = lambda _, x: True
@@ -28,16 +39,23 @@ class FroMaAstTransformer(Transformer):
     # yaml_block = list
     # yaml_block = lambda _, r: print("yaml block details", type(r), r)
     # kvpair = lambda self, pair: print("kvpair details:", type(pair), pair)
-    kvpair = lambda self, pair: dict( [tuple(pair)] ) # List of couple (touple of 2)
+    # kvpair = lambda self, pair: dict( [tuple(pair)] ) # List of couple (touple of 2)
+    # kvpair = lambda self, pair: print(f"kv-details: p0: {type(pair[0])}, {pair[0]} len: {len(pair[0])}\n {type(pair)}, {pair}") or { pair[0]: pair[1] } 
+    kvpair = lambda self, pair: { pair[0]: pair[1] } 
     inline_seq = lambda self, s: s[1:-1].split(',')
+    empty_file = lambda _, x: {'frontmatter': {}, 'body': x}
+    no_metadata = lambda _,x: {'frontmatter': {}, 'body': x[0]['header'] + (x[1]['body'] if len(x) > 2 else '')}
+    # yaml_block = lambda _, list_of_props: { 'frontmatter': list_of_props[0] }
     # yaml_block = lambda _, list_of_props: { 'frontmatter': list_of_props }
     yaml_block = lambda _, list_of_props: { 
         'frontmatter': { 
             k:v for obj in list_of_props for k,v in obj.items() 
     }}
+    # body = lambda self, lines: print(f"BODY-DETAILS: {type(lines)}, {len(lines)}, {type(''.join(lines))}") or { "body" : "".join(lines) }
     body = lambda self, lines: { "body" : "".join(lines) }
     # body = lambda self, lines: "".join(lines)
     # frontmatter = list
+    # start = lambda _, content: content
     # start = lambda _, content: { k:v for k, v in [items for items in content] }
     start = lambda _, content: { 
         k:v for mapping in content for k,v in mapping.items() 
@@ -72,9 +90,16 @@ if __name__ == '__main__':
                     )
 
     grammar = read_text('./Notes/Find Factor Algorithm.md')
+    # grammar = read_text('./Notes/(+) LESS IS MORE.md')
+    # grammar = read_text('./Notes/ChatGPT logo SVG.md')
+    # grammar = read_text('./Notes/Create images of multiple sizes with Python and PIL.md')
+    # grammar = read_text('./Notes/Curl command for Danbooru API.md')
     tree = mdparser.parse(grammar)
     
+    dbg.debugset = 'fmblock'
     dbg.debug(tree, debug_group='fmblock')
+    dbg.debug(type(tree), debug_group='fmblock')
+    
     dbg.debug(tree.pretty(), debug_group='fmblock')
     fmblock = FroMaAstTransformer().transform(tree)
     dbg.debug(f"{type(fmblock)=}", debug_group='fmblock')
@@ -88,4 +113,6 @@ if __name__ == '__main__':
     # dbg.debug(fmblock.pretty(), debug_group="final-result")
 
     pprint(fmblock)
-    print(fmblock['frontmatter'])
+    # print(fmblock['frontmatter'])
+    # print(fmblock['frontmatter'].keys())
+    # print(fmblock['frontmatter']["['Author']"])
